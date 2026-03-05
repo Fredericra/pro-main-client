3f6e0748b89c62d10cb6a6127594a9daa4535cf2
eric-lunix <eric-lunix@eric-lunix.eric>
2026-03-05 16:47:40 +0300

    card

--- src/Page/Home.vue
+++ src/Page/Home.vue
@@ -1,19 +1,92 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import { storeArticle } from '../Auth/article';
 import { storeToRefs } from 'pinia';
-import { PhoneFilled, Plus,Message, ShoppingBag } from '@element-plus/icons-vue';
+import { PhoneFilled, Plus,Message, ShoppingBag, Promotion, WarnTriangleFilled } from '@element-plus/icons-vue';
+import type { DrawerProps } from 'element-plus';
+import Input from '../Components/Input.vue';
+import { userStore } from '../Auth/Store';
+import { useRouter } from 'vue-router';
 
-
+const route = useRouter()
+const messgetext = ref<string>('');
+const drawer = ref<boolean>(false);
+const dialog = ref<boolean>(false);
+const nameMessage = ref<{name: string, img: string, user: string}>({name:'', img:'', user:''});
+const direction = ref<DrawerProps['direction']>('rtl')
 const store2 = storeArticle()
+const store = userStore()
 const { getCarouselle,getAllArticle} = storeToRefs(store2)
+const { user } = storeToRefs(store)
+
+const message = async(name:{name: string, img: string, user: string})=>{
+  nameMessage.value = name
+  if(user.value?.email === name.user){
+    dialog.value = true
+    return
+  }
+  else if(!user.value){
+    route.push({name:'Login'})
+    return
+  }
+  else{
+    drawer.value = !drawer.value
+  }
+}
 
 onMounted(async()=>{
   await store2.checkAllArticle()
   await store2.checkCarousselle()
+  await store.checkAuth()
 })
 </script>
 <template class="space-y-4">
+  <el-dialog v-model="dialog" title="Message envoyée Soi Meme" width="500" class="text-red-900">
+    <template #header>
+      <div>
+        Vous ne pouvez pas envoyer un message à vous même
+      </div>
+      <div class="flex justify-center items-center">
+        <el-icon :size="50">
+          <warn-triangle-filled/>
+        </el-icon>
+      </div>
+    </template>
+     
+  </el-dialog>
+  <el-drawer v-model="drawer" :direction="direction">
+    <template #header>
+      <div class="flex justify-around items-center gap-4">
+        <div>
+          <span class="font-bold text-lg">Message à 
+            <span class="text-sm text-gray-500 cursor-pointer">
+              {{ nameMessage.name }}
+            </span>
+          </span>
+        </div>
+        <div>
+          <el-avatar :src="nameMessage.img" :size="40"></el-avatar>
+        </div>
+      </div>
+    </template>
+    <template #default>
+      <el-empty></el-empty>
+    </template>
+    <template #footer>
+      <div class="flex items-center">
+        <div class="w-full">
+          <Input v-model="messgetext" placeholder="message ..."/>
+        </div>
+        <div>
+          <el-button type="primary">
+            <el-icon>
+              <Promotion/>
+            </el-icon>
+          </el-button>
+        </div>
+      </div>
+    </template>
+  </el-drawer>
   <el-carousel :interval="4000" arrow="always">
     <el-carousel-item v-for="(value,index) in getCarouselle" :key="index">
       <el-image :src="value.image" fit="fill" class="h-100 w-full"  show-progress>
@@ -65,10 +138,10 @@ onMounted(async()=>{
               </el-button>
             </div>
             <div class="">
-              <el-button type="primary">
-                <el-icon>
+              <el-button type="primary" @click="message({name:value.pro?.nom as string,img:value.pro?.set?.link as string,user:value.user?.email as string})">
+                 <el-icon>
                   <Message/>
-                </el-icon>
+                 </el-icon>
                 <span>Message</span>
               </el-button>
             </div>
